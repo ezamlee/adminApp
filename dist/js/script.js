a5ae83bb807e8c96418009dd71013954f67b1f4b
HBahaa <hebabahaa2208@yahoo.com>
2017-10-08 10:46:31 +0200

    updated

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,31 +1,135 @@
-var map, markers = {}, tile_layer, layers = {}; // global variables
+var map, markers = {}, tile_layer, layers = {}, marker, unitID, unitEventID, polyline, mapWaypts = []; // global variables
 
 // Print message to log
 function msg(text) { $("#log").prepend(text + "<br/>"); }
 
 function init() { // Execute after login succeed
 	var sess = wialon.core.Session.getInstance(), // get instance of current Session
-		flags = wialon.item.Item.dataFlag.base | wialon.item.Unit.dataFlag.lastMessage, // specify what kind of data should be returned
+		flags = wialon.item.Item.dataFlag.base | wialon.item.Resource.dataFlag.zones, // specify what kind of data should be returned
 		renderer = wialon.core.Session.getInstance().getRenderer();
 	
 	renderer.addListener("changeVersion", update_renderer);
 	sess.loadLibrary("itemIcon"); // load Icon Library 
-	
+	sess.loadLibrary("resourceZones"); // load Geofences Library
+
 	sess.updateDataFlags( // load items to current session
-	    [{type: "type", data: "avl_unit", flags: flags, mode: 0}], // Items specification
+	    [{type: "type", data: "user", flags: flags, mode: 0},{type: "type", data: "avl_unit", flags: flags, mode: 0},{type: "type", data: "avl_resource", flags: flags, mode: 0}], // Items specification
 	    function (code) { // updateDataFlags callback
 		    if (code) { msg(wialon.core.Errors.getErrorText(code)); return; } // exit if error code
 			
 		    var units = sess.getItems("avl_unit"); // get loaded 'avl_resource's items
+		    var users = sess.getItems("user"); // get loaded 'avl_resource's items
+		    var geofences = sess.getItems("avl_resource"); // get loaded 'avl_resource's items
+
+		    $('#unitsNo').text(units.length);
+		    $('#usersNo').text(users.length);
+		    $('#geofencesNo').text(geofences.length);
+
+
 		    if (!units || !units.length){ msg("No units found"); return; } // check if units found
-		    for (var i = 0; i< units.length; i++) // construct Select list using found resources
+		    for (var i = 0; i< units.length; i++){ // construct Select list using found resources
 			    $("#units").append("<option value='"+ units[i].getId() +"'>"+ units[i].getName()+ "</option>");
-		    
+		    	$("#unit").append("<option value='"+ units[i].getId() +"'>"+ units[i].getName()+ "</option>");
+			}
+			// bind action to select change event
+			$("#unit").change( showUnit );
 		    $("#build").click( show_track );  // bind action to select change event
 			$("#tracks").on("click", ".close_btn", delete_track); //click, when need delete current track
 			$("#tracks").on("click", ".unit", focus_track); //click, when need to see any track
 	});
+
 }
+
+
+/*************************** unit trace ************************/
+
+
+function showUnit(){ // show selected unit on map
+	var val = $("#unit").val(); // get selected unit id
+	if (!val) return; // exit if no unit selected
+	var sess = wialon.core.Session.getInstance(); // get instance of current Session
+	// specify what kind of data should be returned
+	var flags = wialon.item.Unit.dataFlag.lastMessage;
+	var unit = null;
+	if (unitID) { // check if we already have previous unit
+		unit = sess.getItem(unitID);
+		sess.updateDataFlags( // remove previous item from current session
+			[{type: "id", data: unitID, flags: flags, mode: 2}], // item specification
+			function(code) {
+				if (code) { msg(wialon.core.Errors.getErrorText(code)); return; }
+			
+				if (unitEventID) unit.removeListenerById(unitEventID); // unbinding event from this unit
+		});
+	}
+
+	unitID = val;
+	unitEventID = null; // empty event ID
+	mapWaypts = []; // remove all old checkpoints if they were here
+
+	sess.updateDataFlags( // load item with necessary flags to current session
+		[{type: "id", data: unitID, flags: flags, mode: 1}], // item specification
+		function(code) {
+			if (code) { msg(wialon.core.Errors.getErrorText(code)); return; }
+
+			unit = wialon.core.Session.getInstance().getItem(val); // get unit by id
+			if(!unit) return; // exit if no unit
+            var position = unit.getPosition(); // get unit position
+			if (!position) return; // exit if no position 
+			if (map) { // check if map created and we can detect position of unit
+
+                var icon = L.icon({
+                    iconUrl: unit.getIconUrl(32),
+                    iconAnchor: [16, 16]
+                });
+                if (!marker) {
+                    marker = L.marker({lat: position.y, lng: position.x}, {icon: icon}).addTo(map);
+                } else {
+                    marker.setLatLng({lat: position.y, lng: position.x});
+                    marker.setIcon(icon);
+                }
+                map.setView({lat: position.y, lng: position.x});
+
+                if (!polyline) {
+                    polyline = L.polyline([{lat: position.y, lng: position.x}], {color: 'blue'}).addTo(map);
+                }
+
+//				if (polyline) polyline.setMap(null); // if there is alreday path of map - remove it
+//				mapWaypts.push(latlon); // adding point to path array
+			}
+
+			msg("You chose unit: " + unit.getName());
+
+			unitEventID = unit.addListener("messageRegistered", showData); // register event when we will receive message
+	});
+}
+
+function showData(event) {
+	var data = event.getData(); // get data from event
+	
+	if (!data.pos) return; // exit if no position 
+
+	var appended = ''; // here we will put all required information
+	var position = { // get unit position 
+		x: data.pos.x,
+		y: data.pos.y
+	};
+	appended += "Position (x: " + data.pos.x + "; y: " + data.pos.y +")"; // printing position
+	if (data.t) appended += ", time " + wialon.util.DateTime.formatTime(data.t, 1); // printing event time (return user time)
+	msg(appended); // writing to log message about this event
+
+	if (map) { // check if map created
+
+        marker.setLatLng({lat: position.y, lng: position.x});
+
+        if (polyline) {
+            polyline.addLatLng({lat: position.y, lng: position.x});
+        }
+        map.setView({lat: position.y, lng: position.x});
+
+	}
+}
+
+/**************************end unit trace *********************/
 
 function show_track () {
 	var unit_id =  $("#units").val(),
@@ -171,6 +275,17 @@ function init_map() {
 		zoomReverse: true, 
 		zoomOffset: -1
 	}).addTo(map);
+
+
+
+
+	// // create a map in the "map" div, set the view to a given place and zoom
+ //    map = L.map('map').setView([53.9, 27.55], 10);
+
+ //    // add an OpenStreetMap tile layer
+ //    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+ //        attribution: '&copy; <a href="http://gurtam.com">Gurtam</a>'
+ //    }).addTo(map);
 }
 
 // execute when DOM ready
